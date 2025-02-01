62915cacaa952a3cda54ed129f3df7d31b514ea6
nabyungho <valoon2@naver.com>
2025-02-01 18:05:34 +0900

    [feat] init

---
+++ src/test/java/com/sharedevvoyage/javablogsample/JavaBlogSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharedevvoyage.javablogsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlogSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
