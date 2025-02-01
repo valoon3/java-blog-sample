fa761abc2cd1b63e1e47c8b202d7a00f06f43e7f
nabyungho <valoon2@naver.com>
2025-02-01 18:00:14 +0900

    [feat] init

---
+++ src/main/java/com/sharedevvoyage/javablogsample/JavaBlogSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sharedevvoyage.javablogsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBlogSampleApplication.class, args);
+    }
+
+}
